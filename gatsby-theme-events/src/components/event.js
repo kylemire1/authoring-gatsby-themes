258cc7835e483802b0e359017808c7fc0dc2e2b3
Kyle Lemire <kyle@elykinnovation.com>
2019-12-09 20:38:45 -0600

    feat: refactored to format event dates more nicely

--- gatsby-theme-events/src/components/event.js
+++ gatsby-theme-events/src/components/event.js
@@ -1,12 +1,42 @@
 import React from "react";
 
+const getdate = (date, { day = true, month = true, year = true } = {}) =>
+  date.toLocaleDateString("en-US", {
+    day: day ? "numeric" : undefined,
+    month: month ? "long" : undefined,
+    year: year ? "numeric" : undefined
+  });
+
+const EventDate = ({ startDate, endDate }) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const isOneDay = start.toDateString() === end.toDateString();
+
+  return (
+    <>
+      <time dateTime={start.toISOString()}>
+        {getdate(start, { year: isOneDay })}
+      </time>
+      {!isOneDay && (
+        <>
+          -
+          <time dateTime={end.toISOString()}>
+            {getdate(end, { month: start.getMonth() !== end.getMonth() })}
+          </time>
+        </>
+      )}
+    </>
+  );
+};
+
 const Event = ({ name, location, url, startDate, endDate }) => (
   <div>
     <h1>
       {name} ({location})
     </h1>
     <p>
-      {startDate}-{endDate}
+      <EventDate startDate={startDate} endDate={endDate} />
     </p>
     <p>
       Website: <a href={url}>{url}</a>
