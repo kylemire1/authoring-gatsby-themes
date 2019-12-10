f20d130ce1861ca3c99a9b4d515c0e485210be22
Kyle Lemire <kyle@elykinnovation.com>
2019-12-09 20:22:56 -0600

    feat: Built event list and single event components. Displaying event info

--- gatsby-theme-events/src/templates/events.js
+++ gatsby-theme-events/src/templates/events.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
-const EventsTemplate = () => <p>TODO build the events page</p>;
+import Layout from "../components/layout";
+import EventList from "../components/event-list";
+
+const EventsTemplate = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allEvent(sort: { fields: startDate, order: ASC }) {
+        nodes {
+          id
+          name
+          startDate
+          endDate
+          location
+          url
+          slug
+        }
+      }
+    }
+  `);
+
+  const events = data.allEvent.nodes;
+
+  return (
+    <Layout>
+      <EventList events={events} />
+    </Layout>
+  );
+};
 
 export default EventsTemplate;
